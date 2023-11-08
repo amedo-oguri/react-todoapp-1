0a421dbcf889aad0afde439a35f32ccc819daca9
oguri-amedo <mungcal.d.lewis@gmail.com>
2023-11-09 07:00:04 +0900

    handleAddTodoにcompleted:falseを追加

--- src/App.js
+++ src/App.js
@@ -14,20 +14,14 @@ function App() {
 
     if (name === "" ) return;
     setTodos((prevTodos) => {
-      return [...prevTodos,{id, name}]
+      return [...prevTodos,{id, name, completed: false}]
     })
     todoNameRef.current.value = '';
   };
 
   const toggleTodo = id => {
-    // アロー関数を使ってsetTodosを呼び出す
     setTodos(todos => todos.map(todo =>
-      // todos配列内の要素に対しmapメソッドで全ての要素に同じ処理を施す
-      todo.id === id? {...todo, completed: !todo.completed}
-      // 引数のidと一致するidが存在する場合
-      // todoをスプレッド構文によりコピーしてtodoのcompletedを反転させる
-      :todo
-      // 一致しない場合は元のオブジェクトを返す
+      todo.id === id? {...todo, completed: !todo.completed}:todo
       ))
   }
 
@@ -36,9 +30,6 @@ function App() {
     <div>
 
       <TodoList todos={todos} toggleTodo={toggleTodo} />
-
-
-      {/* inputの値を取得 */}
       <input ref={todoNameRef} />
       <button onClick={handleAddTodo}>追加</button>
 
